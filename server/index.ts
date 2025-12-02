30e338f6a35807c81d0a8292a528cdce90ea1fb2
tajnygej <doxyii00@gmail.com>
2025-12-02 11:38:07 +0100

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -217,6 +217,10 @@ app.use((req, res, next) => {
   // ALWAYS serve the app on the port specified in the environment variable PORT
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
+  // --- DODAJEMY TESTOWY ENDPOINT PING ---
+app.get("/ping", (_req: Request, res: Response) => {
+  res.send("pong");
+});
   // It is the only port that is not firewalled.
   const port = parseInt(process.env.PORT || "5000", 10);
   httpServer.listen(
