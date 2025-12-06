d684d8e0152410a7c394a3ea055a9c0df27f4738
tajnygej <doxyii00@gmail.com>
2025-12-06 21:05:29 +0100

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,15 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { HelmetProvider } from "react-helmet-async";
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
-  </React.StrictMode>
-);
-
 
 createRoot(document.getElementById("root")!).render(<App />);
